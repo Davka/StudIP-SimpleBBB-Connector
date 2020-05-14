d2401e50ddd24e3e5f35ae43abe09d8bd2780f45
David Siegfried <ds.siegfried@gmail.com>
2020-05-14 14:27:48 +0200

    ajust

--- views/statistics/index.php
+++ views/statistics/index.php
@@ -1,23 +1,26 @@
-<? if(!empty($current_month_complete)) : ?>
-    <section class="contentbox">
-        <header>
-            <h1><?= sprintf(_('Statistik für %s'), strftime('%B'))?></h1>
-        </header>
-        <section class="month-chart">
-            <script>
-                new Chartist.Bar('.month-chart', {
-                    labels: <?= json_encode(array_keys($current_month_complete))?>,
-                    series: <?= json_encode(array_values($current_month_complete))?>
-                }, {
-                    distributeSeries: true,
-                }).on('draw', function(data) {
-                    if(data.type === 'bar') {
-                        data.element.attr({
-                            style: 'stroke-width: 30px'
-                        });
-                    }
-                });
-            </script>
+<section class="bbb-metrics">
+    <? if(!empty($current_month_complete)) : ?>
+        <section class="contentbox">
+            <header>
+                <h1><?= sprintf(_('Statistik für %s'), strftime('%B'))?></h1>
+            </header>
+            <section class="bbb-metrics month-chart">
+                <script>
+                    new Chartist.Bar('.month-chart', {
+                        labels: <?= json_encode(array_keys($current_month_complete))?>,
+                        series: <?= json_encode(array_values($current_month_complete))?>
+                    }, {
+                        distributeSeries: true,
+                    }).on('draw', function(data) {
+                        if(data.type === 'bar') {
+                            data.element.attr({
+                                style: 'stroke-width: 30px'
+                            });
+                        }
+                    });
+                </script>
+            </section>
         </section>
-    </section>
-<? endif?>
+    <? endif?>
+
+</section>
