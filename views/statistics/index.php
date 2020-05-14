39e1d4046ee4f20dc8113d46af34e47f8a70a956
David Siegfried <ds.siegfried@gmail.com>
2020-05-14 15:23:06 +0200

    ajust

--- views/statistics/index.php
+++ views/statistics/index.php
@@ -4,18 +4,29 @@
             <header>
                 <h1><?= sprintf(_('Statistik für %s'), strftime('%B'))?></h1>
             </header>
-            <section class="bbb-metrics month-chart">
+            <section>
+                <div class="bar ct-chart ct-golden-section ct-negative-labels"></div>
+                <div class="pie ct-chart ct-golden-section ct-negative-labels"></div>
                 <script>
-                    new Chartist.Bar('.month-chart', {
+                    var data = {
                         labels: <?= json_encode(array_keys($current_month_complete))?>,
-                        series: <?= json_encode(array_values($current_month_complete))?>
-                    }, {
+                        series: <?= json_encode(array_map('intval', array_values($current_month_complete)))?>
+                    }
+                    console.log(data);
+                    new Chartist.Bar('.bar', data , {
                         distributeSeries: true,
                     }).on('draw', function(data) {
                         if(data.type === 'bar') {
                             data.element.attr({
                                 style: 'stroke-width: 30px'
                             });
+                        }
+                    });
+                    const sum = function(a, b) {return a + b };
+                    new Chartist.Pie('.pie ', {series: data.series} , {
+                        labelInterpolationFnc: function(value) {
+
+                            return Math.round(value / data.series.reduce(sum) * 100) + '%';
                         }
                     });
                 </script>
